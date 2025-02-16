d61472cce01d696d75bec3c95f8fa3fcfe5fe02f
Eliran Shemesh <piro27@gmail.com>
2025-02-16 17:34:46 +0200

    Move navbar to inside client component to be wrapped in Suspense - required by useSearchParams

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import { fetchExpenses } from '@/utils/db';
-import { Navbar } from '@/components/molecules/navbar';
 import PlainSearchableTable from '@/features/PlainSearchableTable';
 
 export default async function Home() {
@@ -8,7 +7,6 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-1 w-full h-full items-center justify-center">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Navbar />
         <PlainSearchableTable items={allExpenses} />
       </main>
     </div>
--- src/features/PlainSearchableTable/index.jsx
+++ src/features/PlainSearchableTable/index.jsx
@@ -1,16 +1,18 @@
 'use client';
 
+import { Suspense, useState } from 'react';
 import Search from '@/features/Search';
 import Table from '@/components/organisms/table';
-import { useState } from 'react';
+import { Navbar } from '@/components/molecules/navbar';
 
 export default function PlainSearchableTable({ items = [] }) {
     const [searchResults, setSearchResults] = useState(items);
 
     return (
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Navbar />
             <Search items={items} onSearch={setSearchResults} />
             <Table rows={searchResults} />
-        </>
+        </Suspense>
     );
 }
