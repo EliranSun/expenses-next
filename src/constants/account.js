2d044733643bca2c7342bd58476862acbb672275
Eliran Shemesh <piro27@gmail.com>
2025-02-21 12:02:16 +0200

    Add new private account to PrivateAccount list

--- src/constants/account.js
+++ src/constants/account.js
@@ -1,10 +1,11 @@
-
 export const PrivateAccount = [
     "3361",
     "4887",
     "170-489748",
-    "500-489746"
+    "500-489746",
+    "768-489741"
 ];
+
 export const SharedAccount = [
     "9325",
     "754-320766",
