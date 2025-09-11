a961268d15e7e52479f2e8f2f073027451baedf5
Eliran Shemesh <piro27@gmail.com>
2025-09-11 10:29:47 +0300

    Better more clear UI!

--- src/app/money/page.js
+++ src/app/money/page.js
@@ -2,9 +2,10 @@ import { MainNavBar } from "@/components/molecules/MainNavBar";
 import { fetchExpenses } from "@/utils/db";
 import { groupExpensesByMonth } from "@/utils";
 import { format } from "date-fns";
-import { PrivateAccounts } from "@/constants/account";
 import classNames from "classnames";
 import { he } from "date-fns/locale";
+import InfoDisplay from "@/components/molecules/info-display";
+import { Categories } from "@/constants";
 
 const Currency = ({ amount, label, col = false }) => {
     return (
@@ -48,54 +49,78 @@ const TopExpenses = ({ expenses }) => {
 export default async function MoneyPage({ searchParams }) {
     const { year, month, account } = await searchParams;
     const existingExpenses = await fetchExpenses({ year, month, account });
-
     const expensesByMonth = groupExpensesByMonth(existingExpenses);
 
-const years = Object.keys(expensesByMonth).map(Number);
-if (years.length === 0) return null;
+    const years = Object.keys(expensesByMonth).map(Number);
+    if (years.length === 0) return null;
 
-const latestYearNum = Math.max(...years);
-const latestYear = String(latestYearNum);
+    const latestYearNum = Math.max(...years);
+    const latestYear = String(latestYearNum);
 
-const monthsObj = expensesByMonth[latestYear] ?? {};
-const monthKeys = Object.keys(monthsObj).map(Number);
-if (monthKeys.length === 0) return null;
+    const monthsObj = expensesByMonth[latestYear] ?? {};
+    const monthKeys = Object.keys(monthsObj).map(Number);
+    if (monthKeys.length === 0) return null;
 
-const latestMonthNum = Math.max(...monthKeys);
-const latestMonth = String(latestMonthNum);
+    const latestMonthNum = Math.max(...monthKeys);
+    const latestMonth = String(latestMonthNum);
 
-const data = monthsObj[latestMonth];
+    const data = monthsObj[latestMonth];
+
+    console.log({ 'data.categoryTotals': data.categoryTotals });
 
     return (
-        <div className="p-8" dir="rtl">
+        <div className="p-4" dir="rtl">
             <MainNavBar />
-              <div className="">
-    <div key={latestYear}>
-      <h1 className="text-2xl font-bold">{latestYear}</h1>
+            <div className="">
+                <div key={latestYear}>
+                    <div className="flex gap-2">
+                        <div
+                            className="w-full flex flex-col font-mono space-y-8 my-8"
+                            key={`${latestYear}-${latestMonth}`}>
+                            <h2 className="text-2xl text-center font-bold">
+                                {format(new Date(latestYearNum, latestMonthNum), "LLL YYY", { locale: he })}
+                            </h2>
 
-      <div className="flex gap-2">
-        <div className="w-full border p-2 flex flex-col font-mono" key={`${latestYear}-${latestMonth}`}>
-          <h2 className="text-lg font-bold">
-            {format(new Date(latestYearNum, latestMonthNum), "LLL", { locale: he })}
-          </h2>
+                            <div className="flex md:flex-row flex-col gap-2">
+                                <InfoDisplay
+                                    amount={data.totalIncome}
+                                    label="הכנסות"
+                                    isVisible
+                                    iconName="coins" />
+                                <InfoDisplay
+                                    amount={data.totalExpenses}
+                                    label="הוצאות"
+                                    isVisible
+                                    iconName="shoppingCart" />
+                                <InfoDisplay
+                                    amount={data.total}
+                                    label="שורה תחתונה"
+                                    isVisible
+                                    iconName="trendUp" />
+                            </div>
 
-          <Currency amount={data.totalIncome} label="הכנסה" />
-          <Currency amount={data.totalExpenses} label="הוצאה" />
-          <Currency amount={data.total} label="סה״כ" />
+                            {/* <Currency amount={data.totalIncome} label="הכנסה" />
+                            <Currency amount={data.totalExpenses} label="הוצאה" />
+                            <Currency amount={data.total} label="סה״כ" /> */}
 
-          <div className="flex flex-col gap-2 border p-2">
-            {Object.entries(data.categoryTotals)
-              .sort((a, b) => a[1] - b[1])
-              .map(([category, amount]) => (
-                <Currency key={category} amount={amount} label={category.slice(0, 4)} />
-              ))}
-          </div>
+                            <div className="flex flex-wrap gap-2">
+                                {Object.entries(data.categoryTotals)
+                                    .sort((a, b) => a[1] - b[1])
+                                    .map(([category, amount]) => (
+                                        <InfoDisplay
+                                            key={category}
+                                            amount={amount}
+                                            label={category.slice(0, 9)}
+                                            isVisible
+                                            emoji={Categories[category].emoji} />
+                                    ))}
+                            </div>
 
-          <TopExpenses expenses={data.expenses} />
-        </div>
-      </div>
-    </div>
-  </div>
+                            <TopExpenses expenses={data.expenses} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/molecules/info-display.jsx
+++ src/components/molecules/info-display.jsx
@@ -1,5 +1,20 @@
+'use client';
+
 import { CurrencyAmount } from "../atoms/currency-amount";
 import classNames from "classnames";
+import { CoinsIcon, ShoppingCartIcon, TrendUpIcon, TrendDownIcon } from "@phosphor-icons/react";
+
+const IconNames = {
+    coins: CoinsIcon,
+    shoppingCart: ShoppingCartIcon,
+    trendUp: TrendUpIcon,
+    trendDown: TrendDownIcon
+};
+
+const Icon = ({ iconName }) => {
+    const IconComponent = IconNames[iconName];
+    return <IconComponent size={32} />;
+};
 
 const InfoDisplay = ({
     label,
@@ -9,31 +24,39 @@ const InfoDisplay = ({
     percentage,
     isVisible = true,
     round = false,
-    icon
-}) => (
-    <div className={classNames("flex items-center", {
-        "bg-white p-8 rounded-xl shadow-md": true,
-        "hidden": !isVisible,
-        "flex-col": !icon,
-        "justify-between": icon
-    })}>
-        {icon && <span>{icon}</span>}
-        <div className="flex flex-col items-center justify-center text-center w-full">
-            <span className="text-sm">{label} {additionalText}</span>
-            <div className="flex items-center gap-2">
-                <CurrencyAmount
-                    amount={amount}
-                    short={round}
-                    isPositive={showColorIndication && amount > 0}
-                    isNegative={showColorIndication && amount < 0} />
-                {percentage &&
-                    !isNaN(percentage) &&
-                    percentage !== Infinity &&
-                    percentage !== -Infinity &&
-                    <span className="text-sm">({percentage}%)</span>}
+    icon,
+    iconName = "",
+    emoji = ""
+}) => {
+    console.log({ iconName });
+
+    return (
+        <div className={classNames("flex items-center", {
+            "bg-white grow p-2 md:p-8 rounded-xl shadow-md": true,
+            "hidden": !isVisible,
+            "flex-col": !icon,
+            "justify-between": icon
+        })}>
+            {icon && <span>{icon}</span>}
+            {iconName && IconNames[iconName] && <span><Icon iconName={iconName} /></span>}
+            {emoji && <span>{emoji}</span>}
+            <div className="flex flex-col items-center justify-center text-center w-full">
+                <span className="text-sm">{label} {additionalText}</span>
+                <div className="flex items-center gap-2">
+                    <CurrencyAmount
+                        amount={amount}
+                        short={round}
+                        isPositive={showColorIndication && amount > 0}
+                        isNegative={showColorIndication && amount < 0} />
+                    {percentage &&
+                        !isNaN(percentage) &&
+                        percentage !== Infinity &&
+                        percentage !== -Infinity &&
+                        <span className="text-sm">({percentage}%)</span>}
+                </div>
             </div>
-        </div>
-    </div >
-);
+        </div >
+    );
+}
 
 export default InfoDisplay;
